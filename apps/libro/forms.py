6da63293373f528174eb7de849ce587faaddba21
alexander747 <elalexander747@gmail.com>
2021-03-18 13:43:02 -0500

    se agrego estilos de form en forms.py para el crear y editar autor

--- apps/libro/forms.py
+++ apps/libro/forms.py
@@ -5,3 +5,42 @@ class AutorForm(forms.ModelForm):
     class Meta:
         model = Autor
         fields = ['nombre', 'apellidos', 'nacionalidad', 'descripcion']
+
+        labels = {
+            'nombre':'Nombre del autor',
+            'apellidos':'Apellidos del autor',
+            'nacionalidad':'Nacionalidad',
+            'descripcion':'Descripción'
+        }
+
+
+        widgets = {
+             'nombre': forms.TextInput(
+                 attrs = {
+                     'class':'form-control',
+                     'placeholder':'Ingrese nombre del autor',
+                     'id':'nombre'
+                 }
+             ),
+             'apellidos': forms.TextInput(
+                 attrs = {
+                     'class':'form-control',
+                     'placeholder':'Ingrese apellidos',
+                     'id':'apellidos'
+                 }
+             ),
+             'nacionalidad': forms.TextInput(
+                 attrs = {
+                     'class':'form-control',
+                     'placeholder':'Ingrese nacionalidad',
+                     'id':'nacionalidad'
+                 }
+             ),
+             'descripcion': forms.Textarea(
+                 attrs = {
+                     'class':'form-control',
+                     'placeholder':'Descripción',
+                     'id':'descripcion'
+                 }
+             )
+        }
